bdef7fc118c73cf20529ed638a15d1ac6cf3168d
2002jaebin-rgb <2002jaebin@gmail.com>
2025-09-26 23:31:08 +0900

    API KEY UPLOAD

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // OPENDART API 인증키를 보관하는 파일
 // 절대 공개 저장소에 올리지 마세요!
 
-const DART_API_KEY = "여기에_본인_API_KEY";  // ★ 본인 키 입력
+const DART_API_KEY = "e38b53f7d3933fd70142cbaaae29d17ef71bf402";  // ★ 본인 키 입력
