69b9d27e5fb70eab35fd83861d3d87826b407309
2002jaebin-rgb <2002jaebin@gmail.com>
2025-09-27 09:50:10 +0900

    API func._2
    / 검색 기능

---
+++ data/corpmap.js
@@ -0,0 +1,9 @@
+[
+    { "name": "삼성전자", "stock_code": "005930", "corp_code": "00126380" },
+    { "name": "카카오", "stock_code": "035720", "corp_code": "00139042" },
+    { "name": "현대차", "stock_code": "005380", "corp_code": "00164742" },
+    { "name": "POSCO홀딩스", "stock_code": "005490", "corp_code": "00126361" },
+    { "name": "NAVER", "stock_code": "035420", "corp_code": "00129090" },
+    { "name": "LG화학", "stock_code": "051910", "corp_code": "00164779" }
+  ]
+  
